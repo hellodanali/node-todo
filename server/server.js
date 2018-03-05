c6912fb01b8e3577c59db6c748030327d89ac1f9
hellodanali <danagotmail@gmail.com>
2018-03-05 14:53:43 -0800

    add testing for routes

--- server/server.js
+++ server/server.js
@@ -20,6 +20,19 @@ app.post('/todos', (req, res) => {
 	);
 });
 
+app.get('/todos', (req, res) => {
+	Todo.find().then(
+		todos => {
+			res.send({ todos });
+		},
+		e => {
+			res.status(400).send(e);
+		}
+	);
+});
+
 app.listen(3030, () => {
 	console.log('Started on port 3030');
 });
+
+module.exports = { app };
