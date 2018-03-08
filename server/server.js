1af61ad4d37b35207cf0c21c4b8748408e465432
hellodanali <danagotmail@gmail.com>
2018-03-07 23:25:46 -0800

    add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
-
+const _ = require('lodash');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
@@ -63,6 +63,27 @@ app.delete('/todos/:id', (req, res) => {
 		.then(todo => {
 			if (!todo) return status(404).send();
 			res.status(200).send({ todo });
+		})
+		.catch(e => res.status(400).send());
+});
+
+app.patch('/todos/:id', (req, res) => {
+	let id = req.params.id;
+	let body = _.pick(req.body, ['text', 'completed']);
+
+	if (!ObjectID.isValid(id)) return res.status(404).send();
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+		.then(todo => {
+			if (!todo) return res.status(404).send();
+			res.send({ todo });
 		})
 		.catch(e => res.status(400).send());
 });
